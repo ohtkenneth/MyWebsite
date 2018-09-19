0d62f4d300c5e3f45a6d3c7b5c84fa441ec7e1b7
Kenneth Oh <ohtkenneth@gmail.com>
2018-09-19 00:34:11 -0700

    Add tech icons to skills section

--- src/client/Components/Profile/ProfileSection.js
+++ src/client/Components/Profile/ProfileSection.js
@@ -12,7 +12,7 @@ class ProfileSection extends Component {
           <div>
             <div className={ styles['carousel-item'] }>
               <h3 className={ styles.text }>Hello, my name is</h3>
-              <h1 className={ styles.text }>Kenneth.</h1>
+              <h1 className={ styles['text-kenneth'] }>Kenneth.</h1>
               <h3 className={ styles.text }>I'm a full-stack web developer.</h3>
             </div>
           </div>

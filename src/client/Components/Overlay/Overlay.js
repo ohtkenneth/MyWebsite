32c1e99e83a3ed48f8c61d38a6fc9c5c5ccb118e
Kenneth Oh <ohtkenneth@gmail.com>
2018-09-18 21:21:08 -0700

    Style profile using carousel

--- src/client/Components/Overlay/Overlay.js
+++ src/client/Components/Overlay/Overlay.js
@@ -11,7 +11,7 @@ const Overlay = ({ info, onRemoveOverlay }) => {
     // add fade out animation
     document.getElementById('overlay').style.animation = 'fade-out 0.3s ease';
     document.getElementById('overlay-card').style.animation = 'slide-down 0.3s ease';
-    setTimeout(() => onRemoveOverlay(), 300);
+    setTimeout(() => onRemoveOverlay(), 250);
   };
 
   return (
--- src/client/Components/Profile/ProfileSection.js
+++ src/client/Components/Profile/ProfileSection.js
@@ -1,30 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import "react-responsive-carousel/lib/styles/carousel.css";
 import { Carousel } from 'react-responsive-carousel';
 import styles from './ProfileSection.css';
 
-const ProfileSection = () => {
-  return (
-    <div id="profile" className={ styles.root }>
-      <div id="intro">
-        <h3 className={ styles.text }>Hello, my name is</h3>
-        <h1 className={ styles.text }>Kenneth.</h1>
-        <h3 className={ styles.text }>I'm a full-stack web developer.</h3>
+class ProfileSection extends Component {
+  render() {
+    return (
+      <div id="profile">
+        <Carousel showArrows={ true } infiniteLoop={ true } showThumbs={ false }>
+          <div>
+            <div className={ styles['carousel-item'] }>
+              <h3 className={ styles.text }>Hello, my name is</h3>
+              <h1 className={ styles.text }>Kenneth.</h1>
+              <h3 className={ styles.text }>I'm a full-stack web developer.</h3>
+            </div>
+          </div>
+          <div>
+            <div className={ styles['carousel-item'] }>
+              hello
+            </div>
+          </div>
+        </Carousel>
       </div>
-    </div>
-    // <div id="profile" className={ styles.root }>
-    //   <Carousel>
-        // <div id="intro">
-        //   <h3 className={ styles.text }>Hello, my name is</h3>
-        //   <h1 className={ styles.text }>Kenneth.</h1>
-        //   <h3 className={ styles.text }>I'm a full-stack web developer.</h3>
-        // </div>
-        // <div id="bio">
-        //   This is a bio
-        // </div>
-    //   </Carousel>
-    // </div>
-  );
-};
+    );
+  }
+}
 
 export default ProfileSection;

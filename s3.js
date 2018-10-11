fc8808f1b8559aa1cbf26c18cc7d67eb257c7995
ohtkenneth <ohtkenneth@gmail.com>
2018-10-10 23:05:30 -0700

    Add env and s3 upload script

---
+++ s3.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const path = require('path');
+const s3 = require('s3');
+
+const client = s3.createClient({
+  s3Options: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
+
+const paramsBundle = {
+  localFile: path.resolve(__dirname, 'src/client/dist/bundle.js'),
+  s3Params: {
+    Bucket: process.env.MY_BUCKET,
+    Key: "bundle.js",
+  },
+};
+
+const paramsStyles = {
+  localFile: path.resolve(__dirname, 'src/client/dist/styles.css'),
+  s3Params: {
+    Bucket: process.env.MY_BUCKET,
+    Key: "styles.css",
+  },
+};
+
+const uploaderBundle = client.uploadFile(paramsBundle);
+uploaderBundle.on('error', (err) => {
+  console.error('Unable to upload:', err.stack);
+});
+uploaderBundle.on('progress', () => {
+  console.log("Progress", uploaderBundle.progressMd5Amount,
+  uploaderBundle.progressAmount, uploaderBundle.progressTotal);
+});
+uploaderBundle.on('end', function() {
+  console.log("Done uploading bundle.js");
+});
+
+const uploaderStyles = client.uploadFile(paramsStyles);
+uploaderStyles.on('error', (err) => {
+  console.error('Unable to upload:', err.stack);
+});
+uploaderStyles.on('progress', () => {
+  console.log("Progress", uploaderStyles.progressMd5Amount,
+  uploaderStyles.progressAmount, uploaderStyles.progressTotal);
+});
+uploaderStyles.on('end', function() {
+  console.log("Done uploading styles.css");
+});
--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   const { headers, method, url } = req;

b88d03fda3c887d54d2779be500ff298ce32e908
Kenneth Oh <ohtkenneth@gmail.com>
2018-10-24 17:53:40 -0700

    Change title to software engineer

--- src/client/Components/Profile/ProfileSection.js
+++ src/client/Components/Profile/ProfileSection.js
@@ -11,7 +11,7 @@ class ProfileSection extends Component {
         <div className={ styles.intro }>
           <h3 className={ styles.text }>Hello, my name is</h3>
           <a href="http://www.linkedin.com/in/kenneth-oh" target="_blank"><h1 className={ styles['text-kenneth'] }>Kenneth.</h1></a>
-          <h3 className={ styles.text }>I'm a full-stack web developer.</h3>
+          <h3 className={ styles.text }>I'm a full-stack software engineer.</h3>
         </div>
         {/* <div className={ styles.personal }>
           <p>Motivated, disciplined, and diligent. I strive to learn and improve myself both in my personal life and professional career.</p>
